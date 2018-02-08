46988deb7c3aa1929716658d3fea6f97af8a3c33
Andrey Fellbs <a.fellbs@msg-sfb.net>
2018-02-08 23:35:50 +0100

    js20 part2

--- js20-prototype/main.js
+++ js20-prototype/main.js
@@ -46,3 +46,15 @@ nextPerson.greet();
 //determine if prototype of
 
 console.log(Person.isPrototypeOf(nextPerson));
+
+//we need to add property only for developers f.e. array skills
+var webDeveloper = Object.create(Person);
+webDeveloper.constructor = function(name, gender, age, skills) {
+    Person.constructor.apply(this, arguments);
+    this.skills = skills || [];
+    return this;
+};
+
+var dev = Object.create(webDeveloper).constructor("Jack", "male", 21, ["c++", "css", "js"]);
+
+console.log(dev.skills);
