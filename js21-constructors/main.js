96b69c79b503de12fe0b61b888da480ef639b810
andreyfellbs <andrey.fellbs@gmail.com>
2018-06-24 23:40:57 +0200

    js-21 proto

--- js21-constructors/main.js
+++ js21-constructors/main.js
@@ -12,10 +12,16 @@ person = new Person("Jack");
 console.log(person.name);
 person.greet();
 
+console.log(person.constructor);
+console.log(Person.prototype.constructor);
+
 anotherPerson = new Person("Bruce");
 console.log(anotherPerson.name);
+anotherPerson.greet();
+
 
 console.log(Person.prototype);
 
 //check if Prototype
 console.log(anotherPerson instanceof Person);
+
